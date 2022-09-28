93703d1af3690f78d5fd55d030fd66ab2abe96b8
Therese Linden <theresemlinden@gmail.com>
2022-09-28 21:49:21 +0200

    removed user array and only one object. Works to log in as Fredrik and alert when not correct username and password (stored in localStorage)

--- code/app.js
+++ code/app.js
@@ -1,43 +1,51 @@
 const signInBtn = document.getElementById('signInBtn');
 
 //users saved in localStorage
-const users = [
-  {
-    username: 'Fredrik',
-    password: '1234',
-  },
-  {
-    username: 'Therese',
-    password: '1234',
-  },
-];
-//JSON
-//JSON.stringify method converts a JavaScript value to a JSON string,
-//JSON.parse() the data becomes a JavaScript object.
+//removed array. kunde inte validera rätt användare.
+//behöver jag göra en loop då? under sign in()?
+const users = {
+  username: 'Fredrik',
+  password: '1234',
+};
 
 // Put object to storage
 localStorage.setItem(users, JSON.stringify(users));
-// Retrive object from storage
-const getUsers = localStorage.getItem(users);
-// create JavaScript object
-const parseUsers = JSON.parse(getUsers);
 
-parseUsers.find(user => {
-  console.log(user.username);
-  if (user.username === 'Therese') {
-    console.log('Användare finns!!!!');
+// TODO: Validate user login. ONLY if stored in localStorage
+// TODO: Show error message if failed to log in
+// TODO: Can I create a separate validation function?
+const signIn = () => {
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+  const getUsers = localStorage.getItem(users);
+  const parseUsers = JSON.parse(getUsers);
+  //const userError = document.getElementById('userError');
+  //const passError = document.getElementById('passError');
+
+  if (username === parseUsers.username && password === parseUsers.password) {
+    const loginPage = document.getElementById('loginPage');
+    loginPage.style.display = 'none';
+
+    const welcomePage = document.getElementById('welcomePage');
+    welcomePage.style.display = 'flex';
+
+    const capitalizeUsername =
+      username.charAt(0).toUpperCase() + username.slice(1);
+
+    document.getElementById(
+      'userFirstName'
+    ).innerHTML = `Hej, ${capitalizeUsername}`;
+
+    toggleHeaderImage();
   } else {
-    console.log('Användaren finns INTE!!!!');
+    alert('something wrong');
   }
-});
+};
 
 // Hide login page and show welcome page when sign in button clicked
-signInBtn.addEventListener('click', () => {
-  const loginPage = document.getElementById('loginPage');
-  loginPage.style.display = 'none';
-
-  const welcomePage = document.getElementById('welcomePage');
-  welcomePage.style.display = 'flex';
+signInBtn.addEventListener('click', event => {
+  event.preventDefault();
+  signIn();
 });
 
 const toggleHeaderImage = () => {
@@ -48,26 +56,27 @@ const toggleHeaderImage = () => {
     : image.setAttribute('src', '../assets/lock.png');
 };
 
-// Get input Value, capitalize first letter
-// ONLY SIGN IN IF USERNAME AND PASSWORD ADDED
-// and if user is created (stored in localstorage)
-const signIn = () => {
-  // if inputValue find i users then ... {
-  //   hej usernamn + logga ut knapp
-  // } else {
-  //   felmeddelande
-  // }
-  const inputUsername = document.querySelector('#username').value;
-  const capitalizeUsername =
-    inputUsername.charAt(0).toUpperCase() + inputUsername.slice(1);
+// const validateSignIn = () => {
+//   const storedUsername = parseUsers.map(user => user.username);
+//   const storedUserPassword = parseUsers.map(user => user.password);
+//   console.log(storedUsername, storedUserPassword);
 
-  document.getElementById(
-    'userFirstName'
-  ).innerHTML = `Hej, ${capitalizeUsername}`;
+//   // TODO: Validate that user exist in users array
+//   // TODO: Validate that user enter correct stored password
+//   // TODO: Show error message if failed
+//   // TODO: Separate error message depending on fail reason
+// };
 
-  toggleHeaderImage();
-};
+const showErrorMessage = () => {};
 
 const signOut = () => {
   location.reload();
 };
+
+const createUser = () => {
+  // TODO: Push new user to localStorage array
+  // TODO: Create flow sign in page -> create page button -> sign in -> weclome page
+};
+
+//JSON.stringify method converts a JavaScript value to a JSON string,
+//JSON.parse() the data becomes a JavaScript object.
